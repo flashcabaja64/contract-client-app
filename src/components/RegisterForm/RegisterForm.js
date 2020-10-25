90a34154db340c3df98e15030727823fb45699a7
Hubert Yang <offspring7890@yahoo.com>
2020-10-25 16:00:00 -0700

    registration form redesign

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,6 +1,6 @@
 import './RegisterForm.css'
 import React, { useState } from 'react';
-import { Form, Container, Button, Card } from 'react-bootstrap';
+import { Form, Container, Button, Card, Row } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
 import useForm from '../../libs/useForm';
 import validateRegister from '../validations/validateRegister';
@@ -12,7 +12,8 @@ const RegisterForm = () => {
     {
       firstName: '',
       lastName: '',
-      email: '',
+      userName: '',
+      primaryEmail: '',
       password: '',
       confirmPass: ''
     },
@@ -26,7 +27,8 @@ const RegisterForm = () => {
     ApiService.postRegister({
       firstName: values.firstName,
       lastName: values.lastName,
-      email: values.email,
+      userName: values.userName,
+      primaryEmail: values.primaryEmail,
       password: values.password
     }).then(data => {
       console.log(data)
@@ -43,83 +45,108 @@ const RegisterForm = () => {
 
   return (
     <Container fluid className="d-flex justify-content-center align-items-center">
-      <Card style={{ width: '25rem' }}>
-        <Card.Body>
-          <Card.Header>Sign-Up</Card.Header>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="firstName">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter First Name"
-                value={values.firstName}
-                onChange={handleChange}
-                maxLength="35"
-                className={`${errors.firstName && 'inputError'}`}
-              > 
-              </Form.Control>
-            </Form.Group>
-            {errors.firstName && <ValidationError message={errors.firstName}/>}
+      <Row className="col-10 col-offset-1">
+        <div className="col-6">
+          <h1>Project Real Estate</h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis maximus lacus, ac suscipit sapien. Pellentesque a consequat mi, ac mattis tellus. Aenean id mollis nisl, vitae cursus magna. Phasellus quam turpis, tempus dignissim dui sit amet, tincidunt mollis velit. Vestibulum sapien tellus, semper non orci ut, auctor ornare erat. Proin volutpat ex et ipsum blandit gravida. Donec varius nibh at erat fermentum mattis. Etiam iaculis a nibh quis malesuada. Nullam quis mauris a ex congue porta vel ac neque. Donec sagittis dui sed tellus commodo vehicula. Nullam id cursus metus. Sed at eros venenatis, pulvinar lectus eu, porttitor tellus.
+          </p>
+          <img src="http://placeimg.com/640/360/any" alt="placeholder" width="100%"/>
+        </div>
+        <div className="col-6 d-flex justify-content-center">
+          <Card style={{ width: '20rem' }}>
+            <Card.Body>
+              <Card.Title>Sign Up for Free</Card.Title>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId="firstName">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter First Name"
+                      value={values.firstName}
+                      onChange={handleChange}
+                      maxLength="35"
+                      className={`${errors.firstName && 'inputError'}`}
+                    > 
+                    </Form.Control>
+                  </Form.Group>
+                  {errors.firstName && <ValidationError message={errors.firstName}/>}
 
-            <Form.Group controlId="lastName">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Last Name"
-                value={values.lastName}
-                onChange={handleChange}
-                maxLength="35"
-                className={`${errors.lastName && 'inputError'}`}
-              >
-              </Form.Control>
-            </Form.Group>
-            {errors.lastName && <ValidationError message={errors.lastName}/>}
-            
-            <Form.Group controlId="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter Email"
-                value={values.email}
-                onChange={handleChange}
-                className={`${errors.email && 'inputError'}`}
-              >
-              </Form.Control>
-            </Form.Group>
-            {errors.email && <ValidationError message={errors.email}/>}
+                  <Form.Group controlId="lastName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter Last Name"
+                      value={values.lastName}
+                      onChange={handleChange}
+                      maxLength="35"
+                      className={`${errors.lastName && 'inputError'}`}
+                    >
+                    </Form.Control>
+                  </Form.Group>
+                  {errors.lastName && <ValidationError message={errors.lastName}/>}
 
-            <Form.Group controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter Password"
-                value={values.password}
-                onChange={handleChange}
-                className={`${errors.password && 'inputError'}`}
-              >
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="confirmPass">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Confirm Password"
-                value={values.confirmPass}
-                onChange={handleChange}
-                className={`${errors.password && 'inputError'}`}
-              >
-              </Form.Control>
-            </Form.Group>
-            {errors.password && <ValidationError message={errors.password}/>}
-            {resStatus !== '' && <ValidationError message={resStatus}/>}
+                  <Form.Group controlId="userName">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter Username"
+                      value={values.userName}
+                      onChange={handleChange}
+                      maxLength="35"
+                      className={`${errors.userName && 'inputError'}`}
+                    > 
+                    </Form.Control>
+                  </Form.Group>
+                  {errors.userName && <ValidationError message={errors.userName}/>}
 
-            <Form.Group className="text-center">
-              <Button variant="primary" type="submit">Register</Button>
-            </Form.Group>
-            <Form.Text muted>Already Registered? <Link to="/login">Login</Link></Form.Text>
-          </Form>
-        </Card.Body>
-      </Card>
+                  <Form.Group controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter Email"
+                      value={values.primaryEmail}
+                      onChange={handleChange}
+                      className={`${errors.primaryEmail && 'inputError'}`}
+                    >
+                    </Form.Control>
+                  </Form.Group>
+                  {errors.primaryEmail && <ValidationError message={errors.primaryEmail}/>}
+
+                  <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Enter Password"
+                      value={values.password}
+                      onChange={handleChange}
+                      className={`${errors.password && 'inputError'}`}
+                    >
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group controlId="confirmPass">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Confirm Password"
+                      value={values.confirmPass}
+                      onChange={handleChange}
+                      className={`${errors.password && 'inputError'}`}
+                    >
+                    </Form.Control>
+                  </Form.Group>
+                  {errors.password && <ValidationError message={errors.password}/>}
+                  {resStatus !== '' && <ValidationError message={resStatus}/>}
+
+                  <Form.Group className="text-center">
+                    <Button block variant="primary" className="submit_btn" type="submit">Register</Button>
+                  </Form.Group>
+                  <Form.Text muted>Already Registered? <Link to="/login">Login</Link></Form.Text>
+              </Form>
+            </Card.Body>
+          </Card>
+        </div>
+      </Row>
     </Container>
   )
 }
--- src/components/validations/validateRegister.js
+++ src/components/validations/validateRegister.js
@@ -2,6 +2,7 @@ export default function validateRegister(values) {
   let errors = {};
   let fName = values.firstName.trim();
   let lName = values.lastName.trim();
+  let user = values.userName.trim();
   let letters = /^[A-Za-z]+$/
   let emails = /\S+@\S+\.\S+/
   //let REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
@@ -9,7 +10,7 @@ export default function validateRegister(values) {
   //First Name validation errors
   if(fName.length < 3) {
     errors.firstName = 'Please enter more than 3 characters';
-  } else if (fName.length === 0) {
+  } else if (fName === '') {
     errors.firstName = 'First Name required'
   } else if (!letters.test(fName)) {
     errors.firstName = 'Please enter alpha characters only'
@@ -17,15 +18,24 @@ export default function validateRegister(values) {
   //Last Name validation errors
   if(lName.length < 3) {
     errors.lastName = 'Please enter more than 3 characters';
-  }
-  if(!letters.test(lName)) {
+  } else if(lName === '') {
+    errors.lastName = 'Last Name required'
+  } else if(!letters.test(lName)) {
     errors.lastName = 'Please enter alpha characters only'
   }
+  //Username validation errors
+  if(user.length < 3) {
+    errors.userName = 'Please enter more than 3 characters';
+  } else if(user === '') {
+    errors.userName = 'Username required'
+  } else if(!letters.test(user)) {
+    errors.userName = 'Please enter alpha characters only'
+  }
   //Email validation errors
-  if(values.email.length === 0) {
-    errors.email = 'Email address is required';
-  } else if(!emails.test(values.email)) {
-    errors.email = 'Email address is invalid';
+  if(values.primaryEmail.length === 0) {
+    errors.primaryEmail = 'Email address is required';
+  } else if(!emails.test(values.primaryEmail)) {
+    errors.primaryEmail = 'Email address is invalid';
   }
   //Password validation errors
   if((values.password.length || values.confirmPass.length) === 0) {
